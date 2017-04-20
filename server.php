2587afd0a16acc59be4b198de8681d7c696299e1
dolphin <dolphin>
2017-04-20 15:53:30 +0800

    setting

--- server.php
+++ server.php
@@ -6,13 +6,13 @@ use EasyWeChat\Foundation\Application;
 
 $options = [
     'debug'  => true,
-    'app_id' => 'your-app-id',
-    'secret' => 'you-secret',
-    'token'  => 'easywechat',
-    // 'aes_key' => null, // 可选
+    'app_id' => 'wx3f57772b43b05ba5',
+    'secret' => '98926008d074d0ead28018fa8c686d32',
+    'token'  => '5MhSkoNZCxC8GNhmMlwITdNyXuebyxeF',
+    'aes_key' => 'tldoRFpZaPndNYMkEY00nmoGCZsKm5W79N9oJBAns8F', // 可选
     'log' => [
         'level' => 'debug',
-        'file'  => '/tmp/easywechat.log', // XXX: 绝对路径！！！！
+        'file'  => '/alidata/www/s_weixin/log/s_weixin.log', // XXX: 绝对路径！！！！
     ],
     //...
 ];
