c4091c1d0c52b31e1efe980cfb7d3ce83bdc3280
dolphin <dolphin>
2017-04-28 14:28:03 +0800

    config

--- config.php
+++ config.php
@@ -7,5 +7,5 @@ return $config = array(
         'appid'=>'wx3f57772b43b05ba5',
         'appsecret'=>'98926008d074d0ead28018fa8c686d32'
     ),
-    'default_reply_msg' => '感谢关注，http://mobie.hbdx.cc/'
+    'default_reply_msg' => '欢迎您光临金宁文化，线上购票优惠多多，点击所有项目购票，去售票处取票即可开始您的激情假日!!!免费 WIFI：openatv，密码：openatv123'
 );
